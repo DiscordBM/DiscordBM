0ee2b61c23478a23fe5dfebb2c29642a8679bc6e
Mahdi Bahrami <github@mahdibm.com>
2022-11-02 21:19:13 +0330

    `_ToleratesDecode` simplifications

--- Sources/DiscordBM/ToleratesDecode Protocol.swift
+++ Sources/DiscordBM/ToleratesDecode Protocol.swift
@@ -5,71 +5,59 @@ import Logging
  doesn't fail just because Discord has added a new value which the enums don't contain yet.
  If `DiscordGlobalConfiguration.enableLoggingDuringDecode` is enabled, this will log
  the new values that were decoded successfully but the enum didn't have a representation for.
+ `_ToleratesDecode` should not be used outside what there already is in this file.
  */
-protocol ToleratesDecode { }
-protocol ToleratesStringDecodeMarker: ToleratesDecode { }
-protocol ToleratesIntDecodeMarker: ToleratesDecode { }
-
-private enum ToleratesDecodeKind {
-    case string
-    case int
-    case none
-    
-    init<D>(type: D.Type, shouldTolerateDecode: Bool) {
-        switch shouldTolerateDecode {
-        case true:
-            switch D.self {
-            case is any ToleratesStringDecodeMarker.Type: self = .string
-            case is any ToleratesIntDecodeMarker.Type: self = .int
-            default: self = .none
-            }
-        case false: self = .none
-        }
-    }
-}
+protocol _ToleratesDecode { }
+protocol ToleratesStringDecodeMarker: _ToleratesDecode { }
+protocol ToleratesIntDecodeMarker: _ToleratesDecode { }
 
 private let toleratesDecodeLogger = DiscordGlobalConfiguration
     .makeDecodeLogger("ToleratesDecodeProtocol")
 
 // MARK: - +KeyedDecodingContainer
 extension KeyedDecodingContainer {
-    func decode<D>(_: Array<D>.Type, forKey key: Key) throws -> Array<D>
-    where D: Decodable {
-        let shouldTolerateDecode = D.self is any ToleratesDecode.Type
-        let toleratesDecodeKind = ToleratesDecodeKind(
-            type: D.self,
-            shouldTolerateDecode: shouldTolerateDecode
-        )
+    func decode<D>(_: Array<D>.Type, forKey key: Key) throws -> Array<D> where D: Decodable {
         
         var elements = Array<D>()
         var container = try self.nestedUnkeyedContainer(forKey: key)
+        /// Knowing that the internal decodes of this library don't really fail that much,
+        /// I don't know why I shouldn't reserve the capacity beforehand,
+        /// although Foundation doesn't do this.
+        if let count = container.count {
+            elements.reserveCapacity(count)
+        }
         while !container.isAtEnd {
             do {
                 let element = try container.decode(D.self)
                 elements.append(element)
             } catch {
-                switch toleratesDecodeKind {
-                case .none: break
-                case .string:
-                    if let value = try? container.decode(String.self) {
-                        toleratesDecodeLogger.warning("Found a new enum value", metadata: [
-                            "newKey": .string(value),
-                            "decodedSoFar": .stringConvertible(elements),
-                            "totalCount": .stringConvertible(container.count ?? -1),
-                            "type": .string(_typeName(D.self))
-                        ])
-                        continue
+                switch D.self {
+                case is any _ToleratesDecode.Type:
+                    switch D.self {
+                    case is any ToleratesStringDecodeMarker.Type:
+                        if let value = try? container.decode(String.self) {
+                            toleratesDecodeLogger.warning("Found a new enum value", metadata: [
+                                "newKey": .string(value),
+                                "decodedSoFar": .stringConvertible(elements),
+                                "totalCount": .stringConvertible(container.count ?? -1),
+                                "type": .string(_typeName(D.self))
+                            ])
+                            continue
+                        }
+                    case is any ToleratesIntDecodeMarker.Type:
+                        if let value = try? container.decode(Int.self) {
+                            toleratesDecodeLogger.warning("Found a new enum value", metadata: [
+                                "newKey": .stringConvertible(value),
+                                "decodedSoFar": .stringConvertible(elements),
+                                "totalCount": .stringConvertible(container.count ?? -1),
+                                "type": .string(_typeName(D.self))
+                            ])
+                            continue
+                        }
+                    default:
+                        toleratesDecodeLogger.warning("Unhandled marker protocol that conforms to '_ToleratesDecode'. This is a programming error", metadata: ["type": .string(_typeName(D.self))])
                     }
-                case .int:
-                    if let value = try? container.decode(Int.self) {
-                        toleratesDecodeLogger.warning("Found a new enum value", metadata: [
-                            "newKey": .stringConvertible(value),
-                            "decodedSoFar": .stringConvertible(elements),
-                            "totalCount": .stringConvertible(container.count ?? -1),
-                            "type": .string(_typeName(D.self))
-                        ])
-                        continue
-                    }
+                default: break
                 }
                 throw error
             }
