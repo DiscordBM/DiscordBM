34cd5a4b9ccd16f2e54c48abd99945e8d4e4681d
Mahdi Bahrami <github@mahdibm.com>
2022-12-02 08:20:41 +0330

    add tests

---
+++ Tests/DiscordBMTests/DiscordUtils.swift
@@ -0,0 +1,95 @@
+import DiscordBM
+import XCTest
+
+class DiscordUtilsTests: XCTestCase {
+    
+    func test() {
+        do {
+            let id = "\(Int.random(in: 1_000_000_000_000_000...10_000_000_000_000_000))"
+            let string = DiscordUtils.userMention(id: id)
+            XCTAssertEqual(string, "<@\(id)>")
+        }
+        
+        do {
+            let id = "\(Int.random(in: 1_000_000_000_000_000...10_000_000_000_000_000))"
+            let string = DiscordUtils.channelMention(id: id)
+            XCTAssertEqual(string, "<#\(id)>")
+        }
+        
+        do {
+            let id = "\(Int.random(in: 1_000_000_000_000_000...10_000_000_000_000_000))"
+            let string = DiscordUtils.roleMention(id: id)
+            XCTAssertEqual(string, "<@&\(id)>")
+        }
+        
+        do {
+            let id = "\(Int.random(in: 1_000_000_000_000_000...10_000_000_000_000_000))"
+            let string = DiscordUtils.slashCommand(name: "abcd", id: id)
+            XCTAssertEqual(string, "</abcd:\(id)>")
+        }
+        
+        do {
+            let id = "\(Int.random(in: 1_000_000_000_000_000...10_000_000_000_000_000))"
+            let string = DiscordUtils.slashCommand(name: "polikj", id: id, subcommand: "hey")
+            XCTAssertEqual(string, "</polikj hey:\(id)>")
+        }
+        
+        do {
+            let id = "\(Int.random(in: 1_000_000_000_000_000...10_000_000_000_000_000))"
+            let string = DiscordUtils.slashCommand(
+                name: "bhjksa",
+                id: id,
+                subcommand: "poi",
+                subcommandGroup: "homdas"
+            )
+            XCTAssertEqual(string, "</bhjksa homdas poi:\(id)>")
+        }
+        
+        do {
+            let string = DiscordUtils.standardUnicodeEmoji(emoji: "🥳")
+            XCTAssertEqual(string, "🥳")
+        }
+        
+        do {
+            let id = "\(Int.random(in: 1_000_000_000_000_000...10_000_000_000_000_000))"
+            let string = DiscordUtils.customEmoji(name: "qwerrt", id: id)
+            XCTAssertEqual(string, "<:qwerrt:\(id)>")
+        }
+        
+        
+        do {
+            let id = "\(Int.random(in: 1_000_000_000_000_000...10_000_000_000_000_000))"
+            let string = DiscordUtils.customAnimatedEmoji(name: "pwqoe", id: id)
+            XCTAssertEqual(string, "<a:pwqoe:\(id)>")
+        }
+        
+        
+        do {
+            let date = Date()
+            let string1 = DiscordUtils.timestamp(date: date)
+            let string2 = DiscordUtils.timestamp(unixTimestamp: date.timeIntervalSince1970)
+            let string3 = DiscordUtils.timestamp(unixTimestamp: Int(date.timeIntervalSince1970))
+            XCTAssertEqual(string1, "<t:\(Int(date.timeIntervalSince1970))>")
+            XCTAssertEqual(string1, string2)
+            XCTAssertEqual(string2, string3)
+            XCTAssertEqual(string3, string1)
+        }
+        
+        do {
+            let date = Date()
+            let string1 = DiscordUtils.timestamp(date: date, style: .relativeTime)
+            let string2 = DiscordUtils.timestamp(
+                unixTimestamp: date.timeIntervalSince1970,
+                style: .relativeTime
+            )
+            let string3 = DiscordUtils.timestamp(
+                unixTimestamp: Int(date.timeIntervalSince1970),
+                style: .relativeTime
+            )
+            XCTAssertEqual(string1, "<t:\(Int(date.timeIntervalSince1970)):R>")
+            XCTAssertEqual(string1, string2)
+            XCTAssertEqual(string2, string3)
+            XCTAssertEqual(string3, string1)
+        }
+    }
+}
