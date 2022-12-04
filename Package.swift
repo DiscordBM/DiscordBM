6e8a093b51e4fa8c86c4f81e82cdd9c50266ded2
Mahdi Bahrami <github@mahdibm.com>
2022-12-04 06:51:52 +0330

    add iOS/tvOS/watchOS support

--- Package.swift
+++ Package.swift
@@ -1,12 +1,14 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DiscordBM",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v12),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
         .library(
