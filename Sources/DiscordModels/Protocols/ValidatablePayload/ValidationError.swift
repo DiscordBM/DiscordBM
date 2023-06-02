e941da140468d552d8679f93ece954aee148837d
Mahdi Bahrami <github@mahdibm.com>
2023-06-02 21:02:07 +0330

    remove redundant conformance of `Error` types to `Sendable`

--- Sources/DiscordModels/Protocols/ValidatablePayload/ValidationError.swift
+++ Sources/DiscordModels/Protocols/ValidatablePayload/ValidationError.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct ValidationError: Sendable, Error, CustomStringConvertible {
+public struct ValidationError: Error, CustomStringConvertible {
     /// The model that failed the validations.
     public let model: any Sendable
     /// The failed validations. Will never be empty.
