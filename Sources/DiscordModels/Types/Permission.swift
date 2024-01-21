f8233c0dffb1357ee0426170e9aac46b58144fb3
Mahdi Bahrami <github@mahdibm.com>
2024-01-21 22:23:54 +0330

    Add 43 and 44 to `Permission` (#63)

--- Sources/DiscordModels/Types/Permission.swift
+++ Sources/DiscordModels/Types/Permission.swift
@@ -44,6 +44,8 @@ public enum Permission: UInt, Sendable, Hashable, Codable {
     case moderateMembers = 40
     case viewCreatorMonetizationAnalytics = 41
     case useSoundboard = 42
+    case createGuildExpressions = 43
+    case createEvents = 44
     case useExternalSounds = 45
     case sendVoiceMessages = 46
 }
