f3e107eb8ccd305dac7f554a11eda12385d6c939
MahdiBM <github@mahdibm.com>
2024-05-02 17:33:50 +0330

    Move `LosslessRawRepresentable` out of `UnstableEnum` macro target in prep for Swift 6

--- Sources/DiscordModels/UnstableEnumMacro.swift
+++ Sources/DiscordModels/UnstableEnumMacro.swift
@@ -34,4 +34,9 @@ import UnstableEnumMacro
 )
 macro UnstableEnum() = #externalMacro(module: "UnstableEnumMacro", type: "UnstableEnum")
 
-public typealias LosslessRawRepresentable = UnstableEnumMacro.LosslessRawRepresentable
+/// Just a compile-time protocol to make sure `UnstableEnumMacro` types are safe to
+/// force-unwrap when using ``RawRepresentable.init(rawValue:)``
+///
+/// This is assigned to all types with `UnstableEnumMacro` attribute.
+@_marker
+public protocol LosslessRawRepresentable { }
