1147901432c0c500c3ccb0cf52ef3b0c0bd14b4f
MahdiBM <github@mahdibm.com>
2024-07-05 13:32:14 +0330

    Fix test function in Swift 6

--- Tests/DiscordBMTests/+XCTestCase.swift
+++ Tests/DiscordBMTests/+XCTestCase.swift
@@ -2,7 +2,7 @@ import XCTest
 
 extension XCTestCase {
     func waitFulfill(for expectations: [XCTestExpectation], timeout: Double) async {
-#if canImport(Darwin)
+#if canImport(Darwin) || compiler(>=6.0)
         await fulfillment(of: expectations, timeout: timeout)
 #else
         wait(for: expectations, timeout: timeout)
