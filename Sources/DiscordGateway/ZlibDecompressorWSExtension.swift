6fc125bb1e6a4beb5b4fef8640181491965db1a3
MahdiBM <github@mahdibm.com>
2024-12-19 13:40:51 +0330

    minor fixes

--- Sources/DiscordGateway/ZlibDecompressorWSExtension.swift
+++ Sources/DiscordGateway/ZlibDecompressorWSExtension.swift
@@ -3,6 +3,7 @@ import NIOWebSocket
 import NIOCore
 import CompressNIO
 
+/// Will be used for only 1 WS connection so won't need concurrency guards.
 struct ZlibDecompressorWSExtension: WebSocketExtension, @unchecked Sendable {
     let name = "zlib-stream"
     let decompressor: ZlibDecompressor
@@ -12,22 +13,13 @@ struct ZlibDecompressorWSExtension: WebSocketExtension, @unchecked Sendable {
         self.decompressor = try ZlibDecompressor(algorithm: .zlib, windowSize: 15)
     }
 
-    /// Process frame received from websocket
     func processReceivedFrame(_ frame: WebSocketFrame, context: WebSocketExtensionContext) throws -> WebSocketFrame {
         var frame = frame
-
-        frame.data = try self.decompress(
-            from: &frame.data,
-            reserveCapacity: false
-        )
-
+        frame.data = try self.decompress(from: &frame.data)
         return frame
     }
 
-    func decompress(
-        from frame: inout ByteBuffer,
-        reserveCapacity: Bool
-    ) throws -> ByteBuffer {
+    func decompress(from frame: inout ByteBuffer) throws -> ByteBuffer {
         var buffer = allocator.buffer(
             /// `16_360 = 2^14 - 24`, `24` is accounting for allocation overheads.
             /// This doesn't really do anything as of now, since
